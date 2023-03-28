8600bcd4252567df407c6f8bf4aa4a54eb4d37fe
Robiul Islam <99959790+Robiul0Islam@users.noreply.github.com>
2023-03-28 14:20:14 +0600

    first commit

---
+++ tamplate.js
@@ -0,0 +1,6 @@
+const abc=`productive muslim
+the prophet boi ta onek valo lagse.
+after the prophet kinbo kina vabtesi.
+eivabe alada alada line a likha jay.`
+
+console.log(abc);
---
+++ threedots.js
@@ -0,0 +1,5 @@
+const arr=[1,2,3,4,5];
+const arr1=[5,6,7];
+const arr2=[8,9];
+const allArr=[...arr,...arr1,...arr2];
+console.log(allArr);
